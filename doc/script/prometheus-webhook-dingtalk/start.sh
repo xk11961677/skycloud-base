47f7bd13d238d58e6c252ecf0bbef704a50da6d2
shen11961677@163.com <shenliwei521>
2019-08-02 10:24:30 +0800

    init

--- doc/script/prometheus-webhook-dingtalk/start.sh
+++ doc/script/prometheus-webhook-dingtalk/start.sh
@@ -1,4 +1,3 @@
 #!/bin/env bash
 
-docker run -d -p 8060:8060 --name dingtalk timonwong/prometheus-webhook-dingtalk:v0.3.0 --ding.profile="webhook1=https://oapi.dingtalk.com/robot/send?access_token=1fce6f30df64b818bdbe3bd2a11d2b8cc556652c1c004c03540d6268be5a1a8b"
-127.0.0.1:3000" -e "GF_SECURITY_ADMIN_PASSWORD=sky123" grafana/grafana
+docker run -d -p 8060:8060 --name dingtalk timonwong/prometheus-webhook-dingtalk:v0.3.0 --ding.profile="webhook1=https://oapi.dingtalk.com/robot/send?access_token=xxxxx"
